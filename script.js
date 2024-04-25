eea91c44afb0daed4e3ce60d8f859955b4ef3e00
Ritik Jaat <ritikjat990@gmail.com>
2024-04-25 16:08:32 +0545

    seekbar working

--- script.js
+++ script.js
@@ -1,56 +1,58 @@
 console.log("js linked");
-    let currentSong = new Audio();
+let currentSong = new Audio();
 
-    async function getSongs() {
-        try {
-            let a = await fetch("http://127.0.0.1:3000/test/");
-            let respond = await a.text();
+async function getSongs() {
+    try {
+        let a = await fetch("http://127.0.0.1:3000/test/");
+        let respond = await a.text();
 
-            // Check if response is successful
-            if (!a.ok) {
-                throw new Error(`Server responded with status: ${a.status}`);
-            }
-
-            let div = document.createElement("div");
-            div.innerHTML = respond;
-
-            let as = div.getElementsByTagName("a");
-            let songs = [];
-
-            for (let index = 0; index < as.length; index++) {
-                const element = as[index];
-                // Modify filter condition based on your song link format
-                if (element.href.endsWith(".m4a")) {
-                    songs.push(element.href.split("/test/")[1]);
-                }
-            }
-
-            return songs;
-        } catch (error) {
-            console.error("Error fetching songs:", error);
-            // Handle the error here, maybe display a message to the user
-            return [];  // Return empty array in case of error
+        // Check if response is successful
+        if (!a.ok) {
+            throw new Error(`Server responded with status: ${a.status}`);
         }
-    }
 
-    const playMusic = (track) => {
-        currentSong.src = "/test/" + track;
+        let div = document.createElement("div");
+        div.innerHTML = respond;
+
+        let as = div.getElementsByTagName("a");
+        let songs = [];
+
+        for (let index = 0; index < as.length; index++) {
+            const element = as[index];
+            // Modify filter condition based on your song link format
+            if (element.href.endsWith(".m4a")) {
+                songs.push(element.href.split("/test/")[1]);
+            }
+        }
+
+        return songs;
+    } catch (error) {
+        console.error("Error fetching songs:", error);
+        // Handle the error here, maybe display a message to the user
+        return [];  // Return empty array in case of error
+    }
+}
+
+const playMusic = (track, pause = false) => {
+    currentSong.src = "/test/" + track;
+    if (!pause) {
         currentSong.play();
         play.src = "/assets/pause.svg"
-        document.querySelector(".songinfo").innerHTML = track;
-        // currentSong.volume = 0.25;
     }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track);
+    // currentSong.volume = 0.25;
+}
 
-       // Update duration and remaining time
-       currentSong.addEventListener("loadedmetadata", () => {
-        const duration = currentSong.duration;
-        document.querySelector(".durationOfSong").innerHTML = formatTime(duration);
-        updateRemainingTime();
-    });
+// Update duration and remaining time
+currentSong.addEventListener("loadedmetadata", () => {
+    const duration = currentSong.duration;
+    document.querySelector(".durationOfSong").innerHTML = formatTime(duration);
+    updateRemainingTime();
+});
 
-    currentSong.addEventListener("timeupdate", () => {
-        updateRemainingTime();
-    });
+currentSong.addEventListener("timeupdate", () => {
+    updateRemainingTime();
+});
 
 
 function updateRemainingTime() {
@@ -59,6 +61,7 @@ function updateRemainingTime() {
     const remainingTime = duration - currentTime;
     const formattedRemainingTime = formatTime(remainingTime);
     document.querySelector(".countUptpDuration").innerHTML = formatTime(duration - Math.ceil(remainingTime)) || "0:00";
+    document.querySelector(".circle img").style.right = (remainingTime / duration) * 100 + "%";
 }
 
 
@@ -70,13 +73,14 @@ function formatTime(time) {
 
 
 
-    async function main() {
-        let songs = await getSongs();
-        console.log(songs);
+async function main() {
+    let songs = await getSongs();
+    console.log(songs);
+    playMusic(songs[0], true);
 
-        let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0];
-        for (const song of songs) {
-            songUl.innerHTML = songUl.innerHTML + `<li><img class="albumimg" src="/assets/hplay.svg" alt="">
+    let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUl.innerHTML = songUl.innerHTML + `<li><img class="albumimg" src="/assets/hplay.svg" alt="">
                                 <div class="info">
                                     <div>${song.replaceAll("%20", " ")}</div>
                                     <div></div>
@@ -85,62 +89,62 @@ function formatTime(time) {
                             </div>
 
             </li>`;
-        }
-
-        // Volume bar integration
-        const volumeBar = document.getElementById('volume-bar');
-        const volumeFill = document.getElementById('volume-fill');
-        const volumeHandle = document.getElementById('volume-handle');
-
-        // Set initial volume
-        let volume = currentSong.volume;
-        volumeFill.style.width = (volume * 100) + '%';
-        volumeHandle.style.left = (volumeBar.offsetWidth * volume - volumeHandle.offsetWidth / 2) + 'px';
-
-        // Update volume on click and drag
-        volumeHandle.addEventListener('mousedown', function(event) {
-            event.preventDefault();
-            window.addEventListener('mousemove', handleMove);
-            window.addEventListener('mouseup', handleRelease);
-        });
-
-        function handleMove(event) {
-            let newVolume = (event.clientX - volumeBar.getBoundingClientRect().left) / volumeBar.offsetWidth;
-            if (newVolume < 0) {
-                newVolume = 0;
-            } else if (newVolume > 1) {
-                newVolume = 1;
-            }
-            volume = newVolume;
-            volumeFill.style.width = (volume * 100) + '%';
-            volumeHandle.style.left = (volumeBar.offsetWidth * volume - volumeHandle.offsetWidth / 2) + 'px';
-            currentSong.volume = volume; // Update volume of the audio element
-        }
-
-        function handleRelease() {
-            window.removeEventListener('mousemove', handleMove);
-            window.removeEventListener('mouseup', handleRelease);
-        }
-
-        Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
-            e.addEventListener("click", element => {
-                console.log(e.querySelector(".info").firstElementChild.innerHTML)
-                playMusic(e.querySelector(".info").firstElementChild.innerHTML)
-            })
-        })
-
-        play.addEventListener("click", () => {
-            if (currentSong.paused) {
-                currentSong.play();
-                play.src = "/assets/pause.svg"
-            }
-            else {
-                currentSong.pause();
-                play.src = "/assets/play.svg"
-            }
-        })
     }
 
+    // Volume bar integration
+    const volumeBar = document.getElementById('volume-bar');
+    const volumeFill = document.getElementById('volume-fill');
+    const volumeHandle = document.getElementById('volume-handle');
 
-    
-    main();
+    // Set initial volume
+    let volume = currentSong.volume;
+    volumeFill.style.width = (volume * 100) + '%';
+    volumeHandle.style.left = (volumeBar.offsetWidth * volume - volumeHandle.offsetWidth / 2) + 'px';
+
+    // Update volume on click and drag
+    volumeHandle.addEventListener('mousedown', function (event) {
+        event.preventDefault();
+        window.addEventListener('mousemove', handleMove);
+        window.addEventListener('mouseup', handleRelease);
+    });
+
+    function handleMove(event) {
+        let newVolume = (event.clientX - volumeBar.getBoundingClientRect().left) / volumeBar.offsetWidth;
+        if (newVolume < 0) {
+            newVolume = 0;
+        } else if (newVolume > 1) {
+            newVolume = 1;
+        }
+        volume = newVolume;
+        volumeFill.style.width = (volume * 100) + '%';
+        volumeHandle.style.left = (volumeBar.offsetWidth * volume - volumeHandle.offsetWidth / 2) + 'px';
+        currentSong.volume = volume; // Update volume of the audio element
+    }
+
+    function handleRelease() {
+        window.removeEventListener('mousemove', handleMove);
+        window.removeEventListener('mouseup', handleRelease);
+    }
+
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML)
+        })
+    })
+
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play();
+            play.src = "/assets/pause.svg"
+        }
+        else {
+            currentSong.pause();
+            play.src = "/assets/play.svg"
+        }
+    })
+}
+
+
+
+main();
